7c67b6f8805355864afae51dc322f8025914ab3a
Micah Levine <micah.levine@outlook.com>
2019-02-24 15:50:25 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,8 +1,8 @@
 # code the #greeting method here!
 
 def greeting(name)
-  cleaned_name = name.strip
- puts  "Hello #{cleaned_name}. It's nice to meet you."
+  
+ puts  "Hello #{name.strip}. It's nice to meet you."
   
 end 
 
